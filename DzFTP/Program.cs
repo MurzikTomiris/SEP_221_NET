06071032463a59732db276b31afe55c12379f65b
Томирис <@DESKTOP-18IS745>
2024-08-18 20:26:21 +0500

    Dz

---
+++ DzFTP/Program.cs
@@ -0,0 +1,124 @@
+﻿using FluentFTP;
+using System.Net;
+
+namespace DzFTP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //FTP.DownloadFile();
+            //FTP.DownloadFolder();
+            //FTP.DownloadSeveralFiles();
+            //FTP.UploadFile();
+        }
+
+        class FTP
+        {
+            static string url = "ftp://ftp.dlptest.com/";
+            static string user = "dlpuser";
+            static string password = "rNrKYTX9g7z3RgJRmxWuGHbeu";
+            public static void DownloadFile()
+            {
+                FtpClient client = new FtpClient();
+                client.Host = url;
+                client.Credentials = new NetworkCredential(user, password);
+                client.Connect();
+                var status = client.DownloadFile("C:\\Temp\\MyFTP\\My123.xlsx", "/2024/123.xlsx");
+                Console.WriteLine("DownloadFile");
+
+            }
+
+            public static void UploadFile()
+            {
+                FtpClient client = new FtpClient();
+                client.Host = url;
+                client.Credentials = new NetworkCredential(user, password);
+                client.Connect();
+                var status = client.UploadFile("C:\\Temp\\MyFTP\\My123.xlsx", "/2024/Tomiris.xlsx");
+                Console.WriteLine("UploadFile");
+
+            }
+
+            public static void DownloadFolder()
+            {
+                string remoteFolderPath = "/2024";
+                string localFolderPath = "C:\\Temp\\MyFTP";
+
+                using (FtpClient client = new FtpClient(url))
+                {
+                    client.Credentials = new NetworkCredential(user, password);
+                    client.Connect();
+
+                    if (!Directory.Exists(localFolderPath))
+                    {
+                        Directory.CreateDirectory(localFolderPath);
+                    }
+
+                    client.DownloadDirectory(localFolderPath, remoteFolderPath, FtpFolderSyncMode.Update);
+                    Console.WriteLine($"Downloaded folder: {remoteFolderPath} to {localFolderPath}");
+                }
+            }
+
+            public static void DownloadSeveralFiles()
+            {
+                string[] remoteFilePaths = new string[]
+                    {
+                        "C:\\Temp\\MyFTP\\My123.xlsx",
+                        "C:\\Temp\\MyFTP\\readme.txt"
+                    };
+                string localFolderPath = "C:\\Temp\\MyFTP";
+
+                using (FtpClient client = new FtpClient(url))
+                {
+                    client.Credentials = new NetworkCredential(user, password);
+                    client.Connect();
+
+                    if (!Directory.Exists(localFolderPath))
+                    {
+                        Directory.CreateDirectory(localFolderPath);
+                    }
+
+                    foreach (var remoteFilePath in remoteFilePaths)
+                    {
+                        var fileName = Path.GetFileName(remoteFilePath);
+                        var localFilePath = Path.Combine(localFolderPath, fileName);
+
+                        client.DownloadFile(localFilePath, remoteFilePath);
+                        Console.WriteLine($"Downloaded file: {remoteFilePath} to {localFilePath}");
+                    }
+                }
+            }
+
+            public static void UploadFolder(string localFolderPath, string remoteFolderPath)
+            {
+                using (FtpClient client = new FtpClient(url))
+                {
+                    client.Credentials = new NetworkCredential(user, password);
+                    client.Connect();
+
+                    client.UploadDirectory(localFolderPath, remoteFolderPath, FtpFolderSyncMode.Update);
+                    Console.WriteLine($"Uploaded folder: {localFolderPath} to {remoteFolderPath}");
+                }
+            }
+
+            public static void UploadSeveralFiles(string[] localFilePaths, string remoteFolderPath)
+            {
+                using (FtpClient client = new FtpClient(url))
+                {
+                    client.Credentials = new NetworkCredential(user, password);
+                    client.Connect();
+
+                    foreach (var localFilePath in localFilePaths)
+                    {
+                        var fileName = Path.GetFileName(localFilePath);
+                        var remoteFilePath = Path.Combine(remoteFolderPath, fileName).Replace("\\", "/");
+
+                        client.UploadFile(localFilePath, remoteFilePath);
+                        Console.WriteLine($"Uploaded file: {localFilePath} to {remoteFilePath}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DzHTTP/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Net;
+using System.Text;
+
+namespace DzHTTP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8080/");
+            listener.Start();
+            Console.WriteLine("HTTP сервер запущен. Ожидание подключений...");
+
+            while (true)
+            {
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+                HttpListenerResponse response = context.Response;
+
+                if (request.HttpMethod == "POST")
+                {
+                    using (StreamReader reader = new StreamReader(request.InputStream, request.ContentEncoding))
+                    {
+                        string requestBody = reader.ReadToEnd();
+                        var coefficients = ParseCoefficients(requestBody);
+
+                        if (coefficients != null)
+                        {
+                            var (root1, root2, message) = SolveQuadraticEquation(coefficients.Value.a, coefficients.Value.b, coefficients.Value.c);
+                            string responseString = $"<html><body><h1>{message}</h1><p>Root 1: {root1}</p><p>Root 2: {root2}</p></body></html>";
+                            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+                            response.ContentLength64 = buffer.Length;
+                            response.OutputStream.Write(buffer, 0, buffer.Length);
+                        }
+                        else
+                        {
+                            string responseString = "<html><body><h1>Ошибка: Неверные входные данные</h1></body></html>";
+                            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+                            response.ContentLength64 = buffer.Length;
+                            response.OutputStream.Write(buffer, 0, buffer.Length);
+                        }
+                    }
+                }
+                else
+                {
+                    response.StatusCode = (int)HttpStatusCode.MethodNotAllowed;
+                }
+
+                response.OutputStream.Close();
+            }
+        }
+
+        static (double a, double b, double c)? ParseCoefficients(string requestBody)
+        {
+            try
+            {
+                var parts = requestBody.Split('&');
+                double a = double.Parse(parts[0].Split('=')[1]);
+                double b = double.Parse(parts[1].Split('=')[1]);
+                double c = double.Parse(parts[2].Split('=')[1]);
+                return (a, b, c);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        static (double? root1, double? root2, string message) SolveQuadraticEquation(double a, double b, double c)
+        {
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant > 0)
+            {
+                double root1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                double root2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+                return (root1, root2, "Уравнение имеет два действительных корня:");
+            }
+            else if (discriminant == 0)
+            {
+                double root = -b / (2 * a);
+                return (root, null, "Уравнение имеет один действительный корень:");
+            }
+            else
+            {
+                return (null, null, "Уравнение не имеет действительных корней.");
+            }
+        }
+    }
+}
